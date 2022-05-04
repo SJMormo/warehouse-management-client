007f81a543f99d0e78613382fd6b71aff8b18a9b
SJMormo <sjm91097@gmail.com>
2022-05-04 19:30:49 +0600

    Navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='d-flex justify-content-evenly align-items-center'>
+                <Link className='fs-5 text-decoration-none text-black' to="/home">Home</Link>
+                <Link className='fs-1 text-decoration-none text-black' to="/">Fragrance Outlet</Link>
+                <Link className='fs-5 text-decoration-none text-black' to="/">Login</Link>
+            </nav>
+            <hr />
+
+            {/* if user */}
+            <nav className='d-flex justify-content-evenly align-items-center'>
+                <Link className='fs-5 text-decoration-none text-black' to="/home">Manage Items</Link>
+                <Link className='fs-5 text-decoration-none text-black' to="/">Add Items</Link>
+                <Link className='fs-5 text-decoration-none text-black' to="/">My Items</Link>
+            </nav>
+            <hr />
+        </div>
+    );
+};
+
+export default Header;
