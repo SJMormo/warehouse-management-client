debd5d4c83ce8846a29b56597ad95c5a38fc2572
SJMormo <sjm91097@gmail.com>
2022-05-06 01:32:12 +0600

    logout added and header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,53 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from "react-router-dom";
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 
 
 const Header = () => {
+    const [user, loading] = useAuthState(auth);
+
+    const navigate = useNavigate();
+
+    const logOut = () => {
+        signOut(auth);
+        navigate('/');
+    }
+
+    if (loading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
             <nav className='d-flex justify-content-evenly align-items-center'>
                 <Link className='fs-5 text-decoration-none text-black' to="/home">Home</Link>
                 <Link className='fs-1 text-decoration-none text-black' to="/">Fragrance Outlet</Link>
-                <Link className='fs-5 text-decoration-none text-black' to="/login">Login</Link>
+                {
+                    user ?
+                        <button type='button' onClick={logOut} className='btn btn-link fs-5 text-decoration-none text-black'>Sign Out</button>
+                        :
+                        <Link className='fs-5 text-decoration-none text-black' to="/login">Login</Link>
+                }
             </nav >
-            <hr />
+            <hr className={user ? 'mb-2' : 'mb-0'} />
 
             {/* if user */}
-            <nav className='d-flex justify-content-evenly align-items-center'>
-                <Link className='fs-5 text-decoration-none text-black' to="/home">Manage Items</Link>
-                <Link className='fs-5 text-decoration-none text-black' to="/">Add Items</Link>
-                <Link className='fs-5 text-decoration-none text-black' to="/">My Items</Link>
-            </nav>
-            <hr className='mb-0' />
+            {
+                user ?
+                    <div>
+                        <nav className='d-flex justify-content-evenly align-items-center'>
+                            <Link className='fs-5 text-decoration-none text-black' to="/home">Manage Items</Link>
+                            <Link className='fs-5 text-decoration-none text-black' to="/">Add Items</Link>
+                            <Link className='fs-5 text-decoration-none text-black' to="/">My Items</Link>
+                        </nav>
+                        <hr className='mb-0' />
+                    </div>
+                    :
+                    <></>
+            }
+
         </div >
     );
 };
