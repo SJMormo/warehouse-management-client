83acb643242d118b2d3aaa06d075baa52da7e797
SJMormo <sjm91097@gmail.com>
2022-05-06 16:25:31 +0600

    update functionality added to reduce quantity by one

--- src/components/InventoryDetail/InventoryDetail.js
+++ src/components/InventoryDetail/InventoryDetail.js
@@ -1,18 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const InventoryDetail = () => {
     const { id } = useParams();
-
     const [inventoryDetail, setInventoryDetail] = useState({});
+
+    const { _id, name, img, price, description, supplierName } = inventoryDetail;
+    let { quantity } = inventoryDetail;
+
+    const handleDelivery = () => {
+        quantity = quantity - 1;
+        const updatedQuantity = { name, img, price, quantity, description, supplierName };
+
+        // send data to server
+        fetch(`http://localhost:5000/updateQuantity/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedQuantity)
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result);
+            });
+    }
+
+
     useEffect(() => {
         fetch(`http://localhost:5000/inventory/${id}`)
             .then(res => res.json())
             .then(data => setInventoryDetail(data));
-    }, []);
+    }, [handleDelivery]);
 
-    const { _id, name, img, price, quantity, description, supplierName } = inventoryDetail;
+
+
+
 
     return (
         <div>
@@ -31,7 +55,7 @@ const InventoryDetail = () => {
                     <ListGroupItem>Supplier Name: {supplierName}</ListGroupItem>
                 </ListGroup>
                 <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
+                    <Button onClick={handleDelivery}>Delivered</Button>
                     <Card.Link href="#">Another Link</Card.Link>
                 </Card.Body>
             </Card>
